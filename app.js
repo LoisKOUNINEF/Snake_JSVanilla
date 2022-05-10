108225afe05452a662e5b27bb16096754cf53b38
LoisKOUNINEF <lkouninef@gmail.com>
2022-05-10 16:04:45 +0200

    fix mozilla bug

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press OK to retry !')) {
-      window.location.assign(https://loiskouninef.github.io/Snake_JSVanilla/);
+      window.location.assign(https://loiskouninef.github.io/Snake_JSVanilla/)
     }
     return
   }
