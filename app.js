cbfca3a2837647c1e109cf5fb6cc3057de9d56de
LoisKOUNINEF <lkouninef@gmail.com>
2022-05-10 12:41:04 +0200

    fix retry

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press OK to retry !')) {
-      window.location = 'https://loiskouninef.github.io/Snake_JSVanilla/'
+      location.reload();
     }
     return
   }
