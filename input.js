b3b8997b45aefb02112a7bb09508f5db1550a7f4
LoisKOUNINEF <lkouninef@gmail.com>
2022-05-15 18:10:52 +0200

    Revert "touch controls"
    This reverts commit c6ef2770c1a53fb4aca5693a15b6be120857251a.

--- input.js
+++ input.js
@@ -1,76 +1,26 @@
 let inputDirection = { x: 0, y: 0 }
 let lastInputDirection = { x: 0, y: 0 }
 
-
-function keyboardControls(e) {  switch (e.key) {
-  case 'ArrowUp':
-  moveUp()
-  break
-  case 'ArrowDown':
-  moveDown()
-  break
-  case 'ArrowLeft':
-  moveLeft()
-  break
-  case 'ArrowRight':
-  moveRight()
-  break
-}}
-
-
-
-function touchControls() {
-  if (touchendX < touchstartX) {
-    moveLeft()
+window.addEventListener('keydown', e => {
+  switch (e.key) {
+    case 'ArrowUp':
+      if (lastInputDirection.y !== 0) break
+      inputDirection = { x: 0, y: -1}
+      break
+    case 'ArrowDown':
+      if (lastInputDirection.y !== 0) break
+      inputDirection = { x: 0, y: 1}
+      break
+    case 'ArrowLeft':
+      if (lastInputDirection.x !== 0) break
+      inputDirection = { x: -1, y: 0}
+      break
+    case 'ArrowRight':
+      if (lastInputDirection.x !== 0) break
+      inputDirection = { x: 1, y: 0}
+      break
   }
-
-  if (touchendX > touchstartX) {
-    moveRight()
-  }
-
-  if (touchendY < touchstartY) {
-    moveUp()
-  }
-
-  if (touchendY > touchstartY) {
-    moveDown()
-  }
-
-};
-
-function moveUp() {
-  if (lastInputDirection.y !== 0) {return}
-    inputDirection = { x: 0, y: -1}
-};
-
-function moveRight() {
-  if (lastInputDirection.x !== 0) {return}
-    inputDirection = { x: 1, y: 0}
-};
-
-function moveLeft() {
- if (lastInputDirection.x !== 0) {return}
-  inputDirection = { x: -1, y: 0}
-};
-
-function moveDown() {
-  if (lastInputDirection.y !== 0) {return}
-    inputDirection = { x: 0, y: 1}
-};
-
-window.addEventListener('keydown', keyboardControls);
-
-window.addEventListener('touchstart', function (event) {
-  touchstartX = event.changedTouches[0].screenX;
-  touchstartY = event.changedTouches[0].screenY;
-}, false);
-
-window.addEventListener('touchend', function (event) {
-  touchendX = event.changedTouches[0].screenX;
-  touchendY = event.changedTouches[0].screenY;
-  touchControls();
-}, false);
-
+})
 
 export function getInputDirection() {
   lastInputDirection = inputDirection
